7a6adbb505a62c2216f940051125dd8fdb3dd404
Jost Gombac <gombac.jost@gmail.com>
2021-06-19 12:54:45 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { ActionTestComponent } from './action-test/action-test.component';
 import { FormTestComponent } from './form-test/form-test.component';
 import { CombinedTestComponent } from './combined-test/combined-test.component';
 import { NewPageComponent } from './new-page/new-page.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'new-page', component: NewPageComponent },
   { path: 'actions', component: ActionTestComponent },
   { path: 'forms', component: FormTestComponent },
   { path: 'combined', component: CombinedTestComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
